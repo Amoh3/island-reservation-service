d9ed360ae41023772edcf1f247fb12fd926bdd11
abdelrahman <abdelrahman.oh@gmail.com>
2020-11-09 23:15:07 -0500

    - building api module - fixing package naming not to include - and using _ instead

---
+++ api/src/main/kotlin/dev/neurox/services/island_reservation_service/api/SpringBootApp.kt
@@ -0,0 +1,12 @@
+package dev.neurox.services.island_reservation_service.api
+
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootApp
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBootApp::class.java, *args)
+}
